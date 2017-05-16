8ee7011494665bb8180f53f656b8573c33e2f60a
maoyan <1092459508@qq.com>
2017-05-17 01:08:34 +0800

    use longer middleware

--- ife20-redux/src/main.js
+++ ife20-redux/src/main.js
@@ -1,20 +1,18 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
+import logger from 'redux-logger'
 import AppContainer from './containers/AppContainer';
 
 import inAndOut from './redux/reducers/mainReducer';
 
 import './styles/style.scss'
 
-let store = createStore(inAndOut, {items: [{value : 'todo1', finished : false, time : new Date().getTime(), ready : false}]});
-let next = store.dispatch;
-store.dispatch = action => {
-    console.log('dispatching', action);
-    next(action);
-    console.log('store', store.getState());
-}
+let store = createStore(
+    inAndOut, 
+    {items: [{value : 'todo1', finished : false, time : new Date().getTime(), ready : false}]},
+    applyMiddleware(logger));
 
 document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
 
