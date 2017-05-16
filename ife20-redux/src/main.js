0672b6e70e14203f371be3e11f96c27913f6a5fb
maoyan <1092459508@qq.com>
2017-05-17 01:46:21 +0800

    dev logger

--- ife20-redux/src/main.js
+++ ife20-redux/src/main.js
@@ -2,18 +2,27 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
-import logger from 'redux-logger'
 import AppContainer from './containers/AppContainer';
 
 import inAndOut from './redux/reducers/mainReducer';
 
 import './styles/style.scss'
 
+const middlewares = [];
+
+// @开发模式logger
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+  middlewares.push(logger);
+}
+
+//实例化
 let store = createStore(
     inAndOut, 
     {items: [{value : 'todo1', finished : false, time : new Date().getTime(), ready : false}]},
-    applyMiddleware(logger));
+    applyMiddleware(... middlewares));
 
+//页面字体rem
 document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
 
 render((
